ae234ee87d7b3bf209954ab6fa1992d8a16b8b24
DwarakP <dwarak73@gmail.com>
2026-06-07 10:40:29 +0530

    Update 1_Portfolio_Overview.py

--- pages/1_Portfolio_Overview.py
+++ pages/1_Portfolio_Overview.py
@@ -3,7 +3,11 @@ import pandas as pd
 
 st.title("📊 Portfolio Overview")
 
-df = pd.read_csv("data/investments.csv")
+df = pd.read_csv(
+    "data/investments.csv",
+    encoding="utf-8",
+    on_bad_lines="skip"
+)
 
 st.dataframe(df, use_container_width=True)
 
