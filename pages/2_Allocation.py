fb6077e7e15822895176e3c9fad734fabbc17cbf
DwarakP <dwarak73@gmail.com>
2026-06-07 08:58:34 +0530

    Create 2_Allocation.py

---
+++ pages/2_Allocation.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.title("📈 Allocation")
+
+df = pd.read_csv("Investments.csv")
+
+top = df.sort_values(
+    "Current Value",
+    ascending=False
+)
+
+fig = px.pie(
+    top.head(10),
+    values="Current Value",
+    names="Name",
+    title="Top 10 Holdings"
+)
+
+st.plotly_chart(fig, use_container_width=True)
