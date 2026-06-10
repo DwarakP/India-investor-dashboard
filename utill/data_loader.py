27abb0489194a3631741b77d7e65487d89c43d60
DwarakP <dwarak73@gmail.com>
2026-06-10 21:30:11 +0530

    Update data_loader.py

--- utill/data_loader.py
+++ utill/data_loader.py
@@ -3,12 +3,14 @@ import pandas as pd
 def load_portfolio_data():
 
     equity_df = pd.read_excel("data/equity.xlsx")
-    mf_df = pd.read_excel("data/mutual_fund.xlsx")
-    invit_df = pd.read_excel("data/invit.xlsx")
+    mf_df = pd.read_excel("data/mutual funds.xlsx
+    invit_df = pd.read_excel("data/Bonds.xlsx")
+    etf_df = pd.read_excel("data/ETF.xlsx")
 
     equity_df["Asset Class"] = "Indian Equity"
     mf_df["Asset Class"] = "Mutual Fund"
     invit_df["Asset Class"] = "InvIT"
+    etf_df["Asset Class"] = "ETF"
 
     portfolio_df = pd.concat(
         [equity_df, mf_df, invit_df],
