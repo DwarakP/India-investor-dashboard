bc088afa9b311b4035961584c561114ef52f482c
DwarakP <dwarak73@gmail.com>
2026-06-10 21:32:14 +0530

    Update CEO_dashboard.py

--- pages/CEO_dashboard.py
+++ pages/CEO_dashboard.py
@@ -1,3 +1,8 @@
-from utill.data_loader import load_portfolio_data
+from utili.data_loader import load_portfolio_data
 
 portfolio_df = load_portfolio_data()
+
+st.dataframe(
+    portfolio_df,
+    use_container_width=True
+)
